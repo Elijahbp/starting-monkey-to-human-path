cb5f68225c47501a1b3b317c661d7330bd247e14
elijahbp <elijah.b.i.p@gmail.com>
2017-11-15 23:19:52 +0400

    Add PreferencesConstantManager, and more other

---
+++ PO53.Buchnev.wdad/utils/PreferencesConstantManager.java
@@ -0,0 +1,7 @@
+public interface PreferencesConstantManager {
+    public final static String YES = "yes";
+    public final static String NO = "no";
+    public final static String REGISTRY_ADDRESS = "localhost";
+    public final static String REGISTRY_PORT = "1099";
+    public final static String CLASS_PROVIDER = "http://www.yourhost.free.ru/cp/cp.jar";
+}
